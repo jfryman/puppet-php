18d00c1e343150dcb0af5888f5ab31f2c18c9617
James Fryman <james@frymanet.com>
2012-03-06 10:57:26 -0600

    changing syntax

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class php::params {
-  case $::lsbdistcodename {
-    squeeze: {
+  case $::operatingsystem {
+    debian: {
       $php_packages = ['php5-cli', 'php5-common', 'php5-suhosin', 'libonig2', 'php-pear' ]
       $php_apache_packages = ['apache2-mpm-prefork', 'libapache2-mod-php5']
     }
